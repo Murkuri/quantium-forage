874d9d20387c4fa4bebade698cef15a955aa52a6
Murkuri <mnor0018@student.monash.edu>
2025-07-05 23:09:16 +1000

    Cleaner Task 3
    Made the layout construction more cleaner

--- task3.py
+++ task3.py
@@ -2,23 +2,26 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
-SALES_PATH = "tabulated_sales.csv"
+DATA_PATH = "tabulated_sales.csv"
 
 def visualize():
 
     # Read csv and aggregate sales data for entries that have the same date
-    df = pd.read_csv(SALES_PATH)
+    df = pd.read_csv(DATA_PATH)
     df = df.groupby(['date'], as_index=False).agg({'sales': 'sum'})
 
     # Sort the data so that it is in chronological order
     df = df.sort_values(by=['date'], ascending=True)
 
     # Set up the line chart for the sales of overtime of pink morsels
-    chart = px.line(df, x='date', y='sales', title='Sales by Date')
+    chart = px.line(df, x='date', y='sales', title='Pink Morsel Sales Over Time')
+    visualization = dcc.Graph(id='sales-chart', figure=chart)
+    header = html.H1(children='Pink Morsel Sales')
+    subtitle = html.Div(children='''The total sales for pink morsels and their respective dates.''')
 
     # Create App
     app = Dash()
-    app.layout = html.Div(children=[html.H1(children='Pink Morsel Sales'), html.Div(children='''The total sales for pink morsels and their respective dates.'''),dcc.Graph(id='example-graph', figure=chart)])
+    app.layout = html.Div(children=[header, subtitle, visualization])
     app.run(debug=True)
     pass
 
